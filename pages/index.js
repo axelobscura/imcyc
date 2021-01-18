873603cad518cae4275d36f438b6a88d62860481
axelobscura <axosar@gmail.com>
2021-01-17 18:00:09 -0600

    Textos

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <Layout>
       <main className={styles.main}>
+        <h2 className={styles.entrada}>WEBINARS EN CONCRETO</h2>
         <h1 className={styles.entrada}>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
         <h2 className={styles.entrada}>COMPARTIR EL CONOCIMIENTO</h2>
       </main>
