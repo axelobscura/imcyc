23c9d33782bc6f8faf50b57e066754313a5a8a11
Axel <axosar@gmail.com>
2021-01-18 14:35:31 -0600

    Estilos footer

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <Layout>
-<<<<<<< HEAD
-      <main className="main">
-        HOME
-=======
       <main className={styles.main}>
         <h2 className={styles.entrada}>WEBINARS EN CONCRETO</h2>
         <h1 className={styles.entrada}>INSTITUTO MEXICANO DEL CEMENTO Y DEL CONCRETO A.C.</h1>
         <h2 className={styles.entrada}>COMPARTIR EL CONOCIMIENTO</h2>
->>>>>>> af1470fe18e4ce0cdac19dc3061440fb3e35768f
       </main>
     </Layout>
   )
