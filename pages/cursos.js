e8a64cd12bd6b1721b37782cfdf536a34efcfae7
axelobscura <axosar@gmail.com>
2021-03-14 15:35:15 -0600

    Datos

--- pages/cursos.js
+++ pages/cursos.js
@@ -1,9 +1,24 @@
+import Layout from './components/Layout'
 import content from '../frontaid.content';
+
+import styles from '../styles/Home.module.css'
 
 export default function Cursos(){
     return(
-        <div>
-            {content.title}
-        </div>
+        <Layout>
+            <main className={styles.main}>
+                <div>
+                <h2>{content.title}</h2>
+                </div>
+                {content.cursos.map((curso, i) => {
+                    return(
+                        <>
+                            {curso.titulo}
+                            <div dangerouslySetInnerHTML={{__html: content.curso.contenido}}></div>
+                        </>
+                    )
+                })}
+            </main>
+        </Layout>
     )
 }
