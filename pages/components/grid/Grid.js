829b0e7ddf301893e2dea14a6a7ff50ef37e4e89
Axel <axosar@gmail.com>
2021-03-15 19:33:29 -0600

    Grid styling

--- pages/components/grid/Grid.js
+++ pages/components/grid/Grid.js
@@ -3,7 +3,10 @@ import classes from './Grid.module.css';
 export default function Grid(){
   return(
     <div className={classes.wrapgrid}>
-      <div className={classes.griditem}>1</div>
+      <div className={classes.griditem}>
+        <h1 className="text-right pr-5">IMCYC</h1>
+        <h4 className="text-right pr-5">COMPARTIR EL CONOCIMIENTO</h4>
+      </div>
       <div className={classes.griditem}>2</div>
       <div className={classes.griditem}>
         <div className={classes.test}>1</div>
