76ce8914acf12fccd04412d2e50086c5c72149a4
Axel <axosar@gmail.com>
2021-03-15 20:27:23 -0600

    Gridit

--- pages/components/grid/Grid.js
+++ pages/components/grid/Grid.js
@@ -7,7 +7,9 @@ export default function Grid(){
         <h1 className="text-right pr-5">IMCYC</h1>
         <h4 className="text-right pr-5">COMPARTIR EL CONOCIMIENTO</h4>
       </div>
-      <div className={classes.griditem}>2</div>
+      <div className={classes.griditem}>
+        <h3 className="text-right pr-2">Productos<br/>y Servicios</h3>
+      </div>
       <div className={classes.griditem}>
         <div className={classes.test}>1</div>
         <div className={classes.test}>2</div>
