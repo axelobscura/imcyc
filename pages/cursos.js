d0a43be5fe726862ccee79b07f8961656b705f02
Axel <axosar@gmail.com>
2021-03-16 16:42:23 -0600

    Getting static props

--- pages/cursos.js
+++ pages/cursos.js
@@ -3,13 +3,24 @@ import content from '../frontaid.content';
 
 import styles from '../styles/Home.module.css'
 
-export default function Cursos(){
+export default function Cursos(props){
+
+    const { products } = props;
+
     return(
         <Layout>
             <main className={styles.main}>
                 <div>
                 <h2>{content.title}</h2>
                 </div>
+                <div>
+                    <ul>
+                        {products.map((product) => (
+                            <li key={product.id}>{product.title}</li>
+                        ))}
+                    </ul>
+                </div>
+                {/* 
                 {content.cursos.map((curso, i) => {
                     return(
                         <div key={i}>
@@ -19,7 +30,16 @@ export default function Cursos(){
                         </div>
                     )
                 })}
+                */}
             </main>
         </Layout>
     )
+}
+
+export async function getStaticProps(){
+    return {
+        props: {
+            products: [{ id: 'p1', title: 'Product 1'}]
+        }
+    }
 }
