11f9b1b3b22cda3b1be011eff12a8b7a6a333fe6
Axel <axosar@gmail.com>
2021-03-17 19:57:33 -0600

    Pruebas con la api externa

--- pages/[cid].js
+++ pages/[cid].js
@@ -11,18 +11,22 @@ function cursoDetailPage(props){
   return(
     <div>
       <h1>{loadedCurso.title}</h1>
-      <h2>{loadedCurso.description}</h2>
+      <h2>{loadedCurso.body}</h2>
     </div>
   )
 }
 
 async function getData(){
+  /*
   const fs = require('fs/promises');
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+  */
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
 
-  return data;
+  return posts;
 }
 
 export async function getStaticProps(context){
@@ -35,23 +39,29 @@ export async function getStaticProps(context){
 
   const product = data.products.find(product => product.id === cursoId);
 
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${cursoId}`);
+  const posts = await response.json();
+
+  const encontrado = posts.find((posteid) => posteid.id  === cursoId)
+
+  console.log("Aleluya: " + posts);
+
   if(!product){
     return { notFound: true }
   }
 
   return{
     props: {
-      loadedCurso: product
+      loadedCurso: response
+      //loadedCurso: product
     }
   }
 }
 
 export async function getStaticPaths(){
   const data = await getData();
-  const ids = data.products.map(product => product.id);
-  const pathsWithparams = ids.map((id) => ({ params: {cid: id}}));
-
-  
+  const ids = data.map(product => product.id);
+  const pathsWithparams = ids.map((id) => ({ params: {cid: `'${id}'`}}));
 
   return {
     paths: pathsWithparams,
--- pages/cursos.js
+++ pages/cursos.js
@@ -39,12 +39,17 @@ export default function Cursos(props){
 }
 
 export async function getStaticProps(context){
+    /*
     const fs = require('fs/promises'); // LOOK HERE
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonData = await fs.readFile(filePath);
     const data = JSON.parse(jsonData);
+    */
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
 
-    if(!data){
+
+    if(!posts){
         return{
             redirect: {
                 destination: '/'
@@ -52,13 +57,13 @@ export async function getStaticProps(context){
         }
     }
 
-    if(data.products === 0){
+    if(posts === 0){
         return{ notFound: true }
     }
 
     return {
         props: {
-            products: data.products
+            products: posts
         },
         revalidate: 10
     }
